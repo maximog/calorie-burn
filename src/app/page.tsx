88b01add6d7882699f57b6d027e8550ac216993f
Maximo Gijon <maximo.gijon@gmail.com>
2023-11-23 12:05:18 -0300

    create calorie burn view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,124 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import React, { useState } from "react";
+
+const Home = () => {
+  const [activity, setActivity] = useState("running");
+  const [speed, setSpeed] = useState("");
+  const [distance, setDistance] = useState("");
+  const [weight, setWeight] = useState("");
+  const [caloriesBurned, setCaloriesBurned] = useState<number>(0);
+
+  const calculateCalories = () => {
+    // Convert speed, distance, and weight to numbers
+    const speedValue = parseFloat(speed);
+    const distanceValue = parseFloat(distance);
+    const weightValue = parseFloat(weight);
+
+    // Check if inputs are valid numbers
+    if (isNaN(speedValue) || isNaN(distanceValue) || isNaN(weightValue)) {
+      alert("Please enter valid numbers for speed, distance, and weight.");
+      return;
+    }
+
+    // Convert speed to mph
+    const speedMph = speedValue / 1.60934;
+
+    // Use the provided formulas for calorie calculation
+    let caloriesFormula;
+    switch (activity) {
+      case "walking":
+        caloriesFormula = 0.06 * Math.pow(speedMph, 2) - 0.29 * speedMph + 0.73;
+        break;
+      case "running":
+        caloriesFormula = 0.001 * speedMph + 0.656;
+        break;
+      case "swimming":
+        caloriesFormula = 0.28 * Math.pow(speedMph, 2) - 0.97 * speedMph + 2.73;
+        break;
+      case "biking":
+        caloriesFormula = 0.0003 * Math.pow(speedMph, 2) + 0.0331;
+        break;
+      default:
+        alert(
+          "Invalid activity. Supported activities: walking, running, swimming, biking."
+        );
+        return;
+    }
+
+    // Calculate calories burned
+    const caloriesBurnedValue =
+      (caloriesFormula * weightValue * 2.2 * distanceValue) / 1.60934;
+
+    setCaloriesBurned(caloriesBurnedValue);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="max-w-md mx-auto mt-10 p-6 bg-white border rounded-md shadow-md">
+      <h2 className="text-2xl font-semibold mb-4">Calorie Calculator</h2>
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-600">
+          Activity:
+        </label>
+        <select
+          value={activity}
+          onChange={(e) => setActivity(e.target.value)}
+          className="mt-1 p-2 border rounded-md w-full"
+        >
+          <option value="running">Running</option>
+          <option value="walking">Walking</option>
+          <option value="biking">Biking</option>
+          <option value="swimming">Swimming</option>
+        </select>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-600">
+          Speed (kph):
+        </label>
+        <input
+          type="text"
+          value={speed}
+          onChange={(e) => setSpeed(e.target.value)}
+          className="mt-1 p-2 border rounded-md w-full"
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-600">
+          Distance (km):
+        </label>
+        <input
+          type="text"
+          value={distance}
+          onChange={(e) => setDistance(e.target.value)}
+          className="mt-1 p-2 border rounded-md w-full"
+        />
       </div>
-    </main>
-  )
-}
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-600">
+          Weight (kg):
+        </label>
+        <input
+          type="text"
+          value={weight}
+          onChange={(e) => setWeight(e.target.value)}
+          className="mt-1 p-2 border rounded-md w-full"
+        />
+      </div>
+      <button
+        onClick={calculateCalories}
+        className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
+      >
+        Calculate
+      </button>
+      <div className="mt-4">
+        {caloriesBurned !== null && (
+          <p className="text-green-600">
+            Calories burned for {activity}: {caloriesBurned.toFixed(2)} kcal
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
